d80dd26875435c6c5c0e243ddb310bfc86d8cb67
Marcel Brode <m.brode@shopware.com>
2018-09-05 13:38:25 +0200

    NTR - Fix Typo in Filename

--- Resources/views/administration/src/module/swag-migration/component/swag-migration-loading-screen-success/index.js
+++ Resources/views/administration/src/module/swag-migration/component/swag-migration-loading-screen-success/index.js
@@ -1,5 +1,5 @@
 import { Component } from 'src/core/shopware';
-import template from './swag-migration-loading-screen-succes.html.twig';
+import template from './swag-migration-loading-screen-success.html.twig';
 import './swag-migration-loading-screen-success.less';
 
 Component.register('swag-migration-loading-screen-success', {
