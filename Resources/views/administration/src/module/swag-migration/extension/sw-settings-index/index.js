77cd05bc1b8c67002dca4ba1f2219f3b4314430b
Michael Telgmann <mt@shopware.com>
2018-09-07 14:41:41 +0200

    PT-9581 - Fix migration icon place

--- Resources/views/administration/src/module/swag-migration/extension/sw-settings-index/index.js
+++ Resources/views/administration/src/module/swag-migration/extension/sw-settings-index/index.js
@@ -1,5 +1,6 @@
 import { Component } from 'src/core/shopware';
 import template from './sw-settings-index.html.twig';
+import './sw-settings-index.less';
 
 Component.override('sw-settings-index', {
     template
