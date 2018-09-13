28c045f1e311e2885bfdccb5bda0126549b2a32d
Malte Janz <m.janz@shopware.com>
2018-09-13 15:01:23 +0200

    NTR - Change inline assets to url img assets

--- Resources/views/administration/src/module/swag-migration/component/swag-svg-wizard-credentials-failure/index.js
+++
@@ -1,6 +0,0 @@
-import { Component } from 'src/core/shopware';
-import template from './swag-svg-wizard-credentials-failure.html.twig';
-
-Component.register('swag-svg-wizard-credentials-failure', {
-    template
-});
--- Resources/views/administration/src/module/swag-migration/component/swag-svg-wizard-introduction/index.js
+++
@@ -1,6 +0,0 @@
-import { Component } from 'src/core/shopware';
-import template from './swag-svg-wizard-introduction.html.twig';
-
-Component.register('swag-svg-wizard-introduction', {
-    template
-});
