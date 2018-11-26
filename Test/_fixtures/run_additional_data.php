c189391f100a1609968856fc3972235c7d33704d
Krispin <k.luetjann@shopware.com>
2018-11-26 16:39:40 +0100

    PT-9905 - Add ProgressState service tests

---
+++ Test/_fixtures/run_additional_data.php
@@ -0,0 +1,57 @@
+<?php declare(strict_types=1);
+
+return [
+        'additionalData' => [
+        'entityGroups' => [
+            [
+                'id' => 'categories_products',
+                'progress' => 0,
+                'count' => 45,
+                'targetId' => '20080911ffff4fffafffffff19830531',
+                'target' => 'catalog',
+                'entities' => [
+                    [
+                        'entityName' => 'category',
+                        'entityCount' => 8,
+                    ],
+                    [
+                        'entityName' => 'product',
+                        'entityCount' => 37,
+                    ],
+                ],
+            ],
+
+            [
+                'id' => 'customers_orders',
+                'progress' => 0,
+                'count' => 5,
+                'targetId' => '20080911ffff4fffafffffff19830531',
+                'target' => 'catalog',
+                'entities' => [
+                    [
+                        'entityName' => 'customer',
+                        'entityCount' => 3,
+                    ],
+                    [
+                        'entityName' => 'order',
+                        'entityCount' => 2,
+                    ],
+                ],
+            ],
+
+            [
+                'id' => 'media',
+                'progress' => 0,
+                'count' => 23,
+                'targetId' => '20080911ffff4fffafffffff19830531',
+                'target' => 'catalog',
+                'entities' => [
+                    [
+                        'entityName' => 'media',
+                        'entityCount' => 23,
+                    ],
+                ],
+            ],
+        ],
+    ],
+];
