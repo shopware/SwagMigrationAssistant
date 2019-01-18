d4e55ea561c3c8f8b5656e509b42946ab503d9db
Krispin Lütjann <k.luetjann@shopware.com>
2019-01-18 09:48:16 +0100

    NTR - Fix api general setting api service

--- Resources/views/administration/src/core/service/api/swag-migration-general-setting.api.service.js
+++
@@ -1,9 +0,0 @@
-import ApiService from 'src/core/service/api/api.service';
-
-class MigrationGeneralSettingService extends ApiService {
-    constructor(httpClient, loginService, apiEndpoint = 'swag-migration-general-setting') {
-        super(httpClient, loginService, apiEndpoint);
-    }
-}
-
-export default MigrationGeneralSettingService;
