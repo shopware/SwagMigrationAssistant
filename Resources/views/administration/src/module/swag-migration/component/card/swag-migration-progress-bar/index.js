26c9b1d3f944f8f94b0c9dccb0689c1196354669
Krispin Lütjann <k.luetjann@shopware.com>
2019-03-13 08:56:07 +0100

    PT-10243 - Create migration progress bar

---
+++ Resources/views/administration/src/module/swag-migration/component/card/swag-migration-progress-bar/index.js
@@ -0,0 +1,45 @@
+import { Component } from 'src/core/shopware';
+import template from './swag-migration-progress-bar.html.twig';
+import './swag-migration-progress-bar.scss';
+
+Component.register('swag-migration-progress-bar', {
+    template,
+
+    props: {
+        title: {
+            type: String,
+            default: '',
+            required: false
+        },
+        leftPointDescription: {
+            type: String,
+            default: '',
+            required: false
+        },
+        rightPointDescription: {
+            type: String,
+            default: '',
+            required: false
+        },
+        value: {
+            type: Number,
+            default: 0,
+            required: false
+        },
+        maxValue: {
+            type: Number,
+            default: 100,
+            required: false
+        }
+    },
+
+    computed: {
+        rightPointClasses() {
+            return {
+                'swag-migration-progress-bar__bubble': true,
+                'swag-migration-progress-bar__bubble--disabled': this.value < this.maxValue,
+                'swag-migration-progress-bar__bubble--active': this.value >= this.maxValue
+            };
+        }
+    }
+});
