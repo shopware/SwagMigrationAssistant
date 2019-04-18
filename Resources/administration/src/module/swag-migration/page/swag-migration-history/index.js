fcba658c4129086bf03c80fc85001204a051eabf
Malte Janz <m.janz@shopware.com>
2019-04-18 14:17:37 +0200

    PT-10247 - Polish history table

--- Resources/administration/src/module/swag-migration/page/swag-migration-history/index.js
+++ Resources/administration/src/module/swag-migration/page/swag-migration-history/index.js
@@ -13,6 +13,11 @@ Component.register('swag-migration-history', {
         return {
             isLoading: false,
             migrationRuns: [],
+            migrationDateOptions: {
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit'
+            },
             sortBy: 'createdAt',
             sortDirection: 'DESC',
             oldParams: {}
@@ -22,18 +27,78 @@ Component.register('swag-migration-history', {
     computed: {
         migrationRunStore() {
             return State.getStore('swag_migration_run');
+        },
+
+        migrationColumns() {
+            return this.getMigrationColumns();
         }
     },
 
     methods: {
+        getMigrationColumns() {
+            return [
+                {
+                    property: 'connection.name',
+                    dataIndex: 'connection.name',
+                    label: this.$tc('swag-migration.history.connectionName'),
+                    primary: true,
+                    allowResize: true
+                },
+                {
+                    property: 'environmentInformation.sourceSystemDomain',
+                    dataIndex: 'environmentInformation.sourceSystemDomain',
+                    label: this.$tc('swag-migration.history.shopDomain'),
+                    visible: false,
+                    allowResize: true
+                },
+                {
+                    property: 'environmentInformation.sourceSystemName',
+                    dataIndex: 'environmentInformation.sourceSystemName',
+                    label: this.$tc('swag-migration.history.shopSystem'),
+                    visible: false,
+                    allowResize: true
+                },
+                {
+                    property: 'connection.profile',
+                    dataIndex: 'connection.profileId',
+                    label: this.$tc('swag-migration.history.profileAndGateway'),
+                    allowResize: true
+                },
+                {
+                    property: 'status',
+                    dataIndex: 'status',
+                    label: this.$tc('swag-migration.history.status'),
+                    align: 'center',
+                    allowResize: true
+                },
+                {
+                    property: 'progress',
+                    dataIndex: 'progress',
+                    label: this.$tc('swag-migration.history.selectedData'),
+                    visible: false,
+                    allowResize: true
+                },
+                {
+                    property: 'createdAt',
+                    dataIndex: 'createdAt',
+                    label: this.$tc('swag-migration.history.importDateTime'),
+                    align: 'right',
+                    allowResize: true
+                }
+            ];
+        },
+
         getList() {
             this.isLoading = true;
-            const params = this.getListingParams();
+            const params = this.normalizeListingParams(
+                this.getListingParams()
+            );
+
             if (JSON.stringify(this.oldParams) === JSON.stringify(params)) {
                 // Do not request the data again if the parameters don't change.
                 // For example if the detail window (child route) is opened.
                 this.isLoading = false;
-                return Promise.resolve();
+                return Promise.resolve(this.migrationRuns);
             }
 
             this.oldParams = params;
@@ -41,7 +106,24 @@ Component.register('swag-migration-history', {
                 this.total = response.total;
                 this.migrationRuns = response.items;
                 this.isLoading = false;
+
+                return this.migrationRuns;
             });
+        },
+
+        /**
+         * This will convert string values to int values in the param object.
+         * It is needed because Vue Routers '$router.go(-1)' method will mix up
+         * the types of the original params object for integers to strings.
+         *
+         * @param {Object} params
+         * @returns {Object}
+         */
+        normalizeListingParams(params) {
+            params.limit = parseInt(params.limit, 10);
+            params.page = parseInt(params.page, 10);
+
+            return params;
         },
 
         onMigrateButtonClick() {
