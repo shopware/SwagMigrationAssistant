f49fb5d3d157df5f420b653b725088b603078a78
Julia Leusing <j.leusing@shopware.com>
2019-05-07 18:33:00 +0200

    PT-10444 Add plugin icons and add docu link

--- Resources/administration/src/module/swag-migration/extension/sw-settings-index/index.js
+++ Resources/administration/src/module/swag-migration/extension/sw-settings-index/index.js
@@ -1,5 +1,6 @@
 import { Component } from 'src/core/shopware';
 import template from './sw-settings-index.html.twig';
+import './sw-settings-index.scss';
 
 Component.override('sw-settings-index', {
     template
