8b548751f7104a643d37aacb7f4e6b7001f6de6e
Holger-Thomas Kaßner <h.kassner@shopware.com>
2019-05-14 15:05:56 +0200

    NTR - Fix test

--- Test/Migration/Services/RunServiceTest.php
+++ Test/Migration/Services/RunServiceTest.php
@@ -190,6 +190,7 @@ class RunServiceTest extends TestCase
             new DataSelectionRegistry([]),
             $this->dataRepo,
             $mediaFileRepo,
+            $this->getContainer()->get('currency.repository'),
             $this->getContainer()->get(IndexerRegistry::class),
             $this->getContainer()->get('shopware.cache')
         );
