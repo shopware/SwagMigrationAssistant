6d0c5a0c6539b6caaa0af581a8c1afbb1d6f5029
Malte Janz <m.janz@shopware.com>
2019-07-29 14:50:49 +0200

    PT-10681 - Logging for unsupported document types

---
+++ Migration/Logging/Log/DocumentTypeNotSupported.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types=1);
+
+namespace SwagMigrationAssistant\Migration\Logging\Log;
+
+use SwagMigrationAssistant\Migration\DataSelection\DefaultEntities;
+
+class DocumentTypeNotSupported extends BaseRunLogEntry
+{
+    /**
+     * @var string
+     */
+    private $type;
+
+    public function __construct(string $runId, string $sourceId, string $type)
+    {
+        parent::__construct($runId, DefaultEntities::ORDER_DOCUMENT, $sourceId);
+        $this->type = $type;
+    }
+
+    public function getLevel(): string
+    {
+        return self::LOG_LEVEL_INFO;
+    }
+
+    public function getCode(): string
+    {
+        return 'SWAG_MIGRATION__DOCUMENT_TYPE_NOT_SUPPORTED';
+    }
+
+    public function getTitle(): string
+    {
+        return 'Document type is not supported';
+    }
+
+    public function getParameters(): array
+    {
+        return [
+            'sourceId' => $this->getSourceId(),
+            'type' => $this->type,
+        ];
+    }
+
+    public function getDescription(): string
+    {
+        $args = $this->getParameters();
+
+        return sprintf(
+            'The document with the source id "%s" could not be converted because the document type "%s" is not supported.',
+            $args['sourceId'],
+            $args['type']
+        );
+    }
+}
