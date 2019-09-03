3fb22e910de37e7c8b67098e25107a8b71e82f80
Holger-Thomas Kaßner <h.kassner@shopware.com>
2019-09-03 11:26:46 +0200

    NTR - Adjust tooling

--- bin/phpstan.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-composer dump-autoload
-./../../../vendor/shopware/platform/bin/phpstan.phar analyze --level 5 --configuration phpstan.neon --autoload-file=../../../vendor/autoload.php .
