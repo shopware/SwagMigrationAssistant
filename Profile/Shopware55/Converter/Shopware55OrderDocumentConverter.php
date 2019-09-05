9b0ec0765e94849a53c1612e015e3f89812962be
Holger-Thomas Kaßner <h.kassner@shopware.com>
2019-09-05 14:53:59 +0200

    NTR - Fix order document converter for edge case in 5.5

--- Profile/Shopware55/Converter/Shopware55OrderDocumentConverter.php
+++ Profile/Shopware55/Converter/Shopware55OrderDocumentConverter.php
@@ -2,6 +2,8 @@
 
 namespace SwagMigrationAssistant\Profile\Shopware55\Converter;
 
+use Shopware\Core\Framework\Context;
+use SwagMigrationAssistant\Migration\Converter\ConvertStruct;
 use SwagMigrationAssistant\Migration\MigrationContextInterface;
 use SwagMigrationAssistant\Profile\Shopware\Converter\OrderDocumentConverter;
 use SwagMigrationAssistant\Profile\Shopware\DataSelection\DataSet\OrderDocumentDataSet;
@@ -13,5 +15,16 @@ class Shopware55OrderDocumentConverter extends OrderDocumentConverter
     {
         return $migrationContext->getProfile()->getName() === Shopware55Profile::PROFILE_NAME
             && $migrationContext->getDataSet()::getEntity() === OrderDocumentDataSet::getEntity();
+    }
+
+    public function convert(array $data, Context $context, MigrationContextInterface $migrationContext): ConvertStruct
+    {
+        if (isset($data['ID'])) {
+            $data['id'] = $data['ID'];
+
+            unset($data['ID']);
+        }
+
+        return parent::convert($data, $context, $migrationContext);
     }
 }
