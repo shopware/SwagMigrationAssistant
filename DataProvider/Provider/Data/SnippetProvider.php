84f82ddc20ca9a80f075ce26ac31a21b92094225
Krispin Lütjann <k.luetjann@shopware.com>
2020-11-20 11:48:28 +0100

    MIG-158 - Add migration of snippets and snippet sets

---
+++ DataProvider/Provider/Data/SnippetProvider.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types=1);
+/*
+ * (c) shopware AG <info@shopware.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SwagMigrationAssistant\DataProvider\Provider\Data;
+
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Sorting\FieldSorting;
+use SwagMigrationAssistant\Migration\DataSelection\DefaultEntities;
+
+class SnippetProvider extends AbstractProvider
+{
+    /**
+     * @var EntityRepositoryInterface
+     */
+    private $snippetRepo;
+
+    public function __construct(EntityRepositoryInterface $snippetRepo)
+    {
+        $this->snippetRepo = $snippetRepo;
+    }
+
+    public function getIdentifier(): string
+    {
+        return DefaultEntities::SNIPPET;
+    }
+
+    public function getProvidedData(int $limit, int $offset, Context $context): array
+    {
+        $criteria = new Criteria();
+        $criteria->setLimit($limit);
+        $criteria->setOffset($offset);
+        $criteria->addSorting(new FieldSorting('id'));
+        $result = $this->snippetRepo->search($criteria, $context);
+
+        return $this->cleanupSearchResult($result);
+    }
+
+    public function getProvidedTotal(Context $context): int
+    {
+        return $this->readTotalFromRepo($this->snippetRepo, $context);
+    }
+}
