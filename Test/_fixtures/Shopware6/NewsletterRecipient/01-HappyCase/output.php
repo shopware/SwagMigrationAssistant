a185776eca8b72bce066ec93cb153f8d100a7e84
Max Stegmeyer <m.stegmeyer@shopware.com>
2020-11-26 10:23:34 +0100

    MIG-155 - Add newsletter recipient

---
+++ Test/_fixtures/Shopware6/NewsletterRecipient/01-HappyCase/output.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+/*
+ * (c) shopware AG <info@shopware.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'email' => 'some@one.com',
+    'title' => 'Master',
+    'firstName' => 'Jano',
+    'lastName' => 'Doe',
+    'zipCode' => '12345',
+    'city' => 'Some City',
+    'street' => 'Some Street 5',
+    'status' => 'optIn',
+    'hash' => '2477217a5ae6430ea731c771afd3602c',
+    'salutationId' => '6dd637353d044752ae6a8c6e7f53430b',
+    'languageId' => '5dd637353d044752ae6a8c6e7f53430b',
+    'salesChannelId' => '665b8a01d83841369cf3c58d22481a3d',
+    'confirmedAt' => '2020-11-26T10:09:26.000+00:00',
+    'tags' => [
+        [
+            'name' => 'Tag!',
+            'id' => 'cfdbb85022314529a1e4caa84829e1b8',
+        ],
+    ],
+    'id' => 'de4cfbb530c04e28b1d99ec79c1bda60',
+];
