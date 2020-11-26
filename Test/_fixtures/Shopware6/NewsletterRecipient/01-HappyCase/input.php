1063e9fd29d4faffb494b38836a1b1d6e7d1bca2
Max Stegmeyer <m.stegmeyer@shopware.com>
2020-11-26 10:23:34 +0100

    MIG-155 - Add newsletter recipient
    (cherry picked from commit a185776eca8b72bce066ec93cb153f8d100a7e84)

---
+++ Test/_fixtures/Shopware6/NewsletterRecipient/01-HappyCase/input.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+/*
+ * (c) shopware AG <info@shopware.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'email' => 'some@one.com',
+    'title' => 'Master',
+    'firstName' => 'Jano',
+    'lastName' => 'Doe',
+    'zipCode' => '12345',
+    'city' => 'Some City',
+    'street' => 'Some Street 5',
+    'status' => 'optIn',
+    'hash' => '2477217a5ae6430ea731c771afd3602c',
+    'salutationId' => 'a75917aa2c664872a50992412a40a7ab',
+    'languageId' => '24f78d24f14849f6922ab2f76dcd76e8',
+    'salesChannelId' => '3f05f1f6514d43f7ae11a669c7557d1c',
+    'confirmedAt' => '2020-11-26T10:09:26.000+00:00',
+    'tags' => [
+        [
+            'name' => 'Tag!',
+            'id' => 'cfdbb85022314529a1e4caa84829e1b8',
+        ],
+    ],
+    'id' => 'de4cfbb530c04e28b1d99ec79c1bda60',
+];
---
+++ Test/_fixtures/Shopware6/NewsletterRecipient/01-HappyCase/mapping.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+/*
+ * (c) shopware AG <info@shopware.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use SwagMigrationAssistant\Migration\DataSelection\DefaultEntities;
+
+return [
+    [
+        'entityName' => DefaultEntities::LANGUAGE,
+        'oldIdentifier' => '24f78d24f14849f6922ab2f76dcd76e8',
+        'newIdentifier' => '5dd637353d044752ae6a8c6e7f53430b',
+    ],
+    [
+        'entityName' => DefaultEntities::SALES_CHANNEL,
+        'oldIdentifier' => '3f05f1f6514d43f7ae11a669c7557d1c',
+        'newIdentifier' => '665b8a01d83841369cf3c58d22481a3d',
+    ],
+    [
+        'entityName' => DefaultEntities::SALUTATION,
+        'oldIdentifier' => 'a75917aa2c664872a50992412a40a7ab',
+        'newIdentifier' => '6dd637353d044752ae6a8c6e7f53430b',
+    ],
+];
