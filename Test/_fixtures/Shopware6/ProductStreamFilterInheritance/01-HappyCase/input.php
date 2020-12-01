dd766cf805fa8bc7e066eae4c8d86379fb6c5c87
Max Stegmeyer <m.stegmeyer@shopware.com>
2020-12-01 11:46:26 +0100

    MIG-146 - Add product streams and cross selling
    (cherry picked from commit 1f85ca118ec24f8a0b9f9eb1aaaee6df1eca05b8)

---
+++ Test/_fixtures/Shopware6/ProductStreamFilterInheritance/01-HappyCase/input.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+/*
+ * (c) shopware AG <info@shopware.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'parentId' => 'f2a774f3475d441dbfb487d2c0cdd885',
+    'id' => '3ea8b37a2cd94beaab997db80d3f5ec2',
+];
