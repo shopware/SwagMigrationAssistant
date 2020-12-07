f62c3fbd5113730230c14351daddb3e604aa912e
Krispin Lütjann <k.luetjann@shopware.com>
2020-12-07 13:55:30 +0100

    MIG-152 - Add product review
    (cherry picked from commit fc395863b19afc852a4a52c6881e5d017258d53f)

---
+++ Test/_fixtures/Shopware6/ProductReview/01-HappyCase/output.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+/*
+ * (c) shopware AG <info@shopware.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'productId' => '09f91487bce145d18bc350b5147da2d3',
+    'customerId' => '87e6b1851ed148a6b84296a421e9939b',
+    'salesChannelId' => '98432def39fc4624b33213a56b8c944d',
+    'languageId' => 'ac6e2d5f035a41aea1623189b1751f03',
+    'points' => 3,
+    'status' => false,
+    'content' => 'Consequatur vero laudantium enim facilis quibusdam. Sed iusto excepturi occaecati nesciunt eos ratione pariatur. Beatae et aut quod saepe quasi ut mollitia.',
+    'title' => 'Quia iste sint qui minus.',
+    'id' => '0eff019992ff4ca98497c1ce4543b584',
+];
