03e7bb840a29aaa856a233dc5877c8b0e5c35b8d
Krispin Lütjann <k.luetjann@shopware.com>
2021-05-25 09:47:53 +0200

    MIG-233 - Add api migration of wishlists

---
+++ Profile/Shopware/Gateway/Api/Reader/CustomerWishlistReader.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+/*
+ * (c) shopware AG <info@shopware.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SwagMigrationAssistant\Profile\Shopware\Gateway\Api\Reader;
+
+use SwagMigrationAssistant\Migration\DataSelection\DefaultEntities;
+use SwagMigrationAssistant\Migration\MigrationContextInterface;
+use SwagMigrationAssistant\Profile\Shopware\Gateway\Api\ShopwareApiGateway;
+use SwagMigrationAssistant\Profile\Shopware\ShopwareProfileInterface;
+
+class CustomerWishlistReader extends ApiReader
+{
+    public function supports(MigrationContextInterface $migrationContext): bool
+    {
+        return $migrationContext->getProfile() instanceof ShopwareProfileInterface
+            && $migrationContext->getGateway()->getName() === ShopwareApiGateway::GATEWAY_NAME
+            && $this->getDataSetEntity($migrationContext) === DefaultEntities::CUSTOMER_WISHLIST;
+    }
+
+    protected function getApiRoute(): string
+    {
+        return 'SwagMigrationOrderNotes';
+    }
+}
