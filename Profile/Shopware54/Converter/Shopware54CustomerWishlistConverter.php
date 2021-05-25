ea032d7712a7160700495d0b0e0ed92dc053a54b
Krispin Lütjann <k.luetjann@shopware.com>
2021-05-25 09:47:53 +0200

    MIG-233 - Add api migration of wishlists
    (cherry picked from commit 03e7bb840a29aaa856a233dc5877c8b0e5c35b8d)

---
+++ Profile/Shopware54/Converter/Shopware54CustomerWishlistConverter.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+/*
+ * (c) shopware AG <info@shopware.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SwagMigrationAssistant\Profile\Shopware54\Converter;
+
+use SwagMigrationAssistant\Migration\MigrationContextInterface;
+use SwagMigrationAssistant\Profile\Shopware\Converter\CustomerWishlistConverter;
+use SwagMigrationAssistant\Profile\Shopware\DataSelection\DataSet\CustomerWishlistDataSet;
+use SwagMigrationAssistant\Profile\Shopware54\Shopware54Profile;
+
+class Shopware54CustomerWishlistConverter extends CustomerWishlistConverter
+{
+    public function supports(MigrationContextInterface $migrationContext): bool
+    {
+        return $migrationContext->getProfile()->getName() === Shopware54Profile::PROFILE_NAME
+            && $this->getDataSetEntity($migrationContext) === CustomerWishlistDataSet::getEntity();
+    }
+}
