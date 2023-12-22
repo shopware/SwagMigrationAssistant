1d8ac6f3827719e3020e6f25521ebcb962eacd5c
Jonas Elfering <j.elfering@shopware.com>
2023-12-22 15:00:00 +0100

    NEXT-30687 - Add symfony 7 compatibility

--- Controller/HistoryController.php
+++ Controller/HistoryController.php
@@ -18,7 +18,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\StreamedResponse;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 #[Route(defaults: ['_routeScope' => ['api']])]
