d795e652b5b7d327029119acf88b0c344ded35c5
Malte Janz <m.janz@shopware.com>
2024-12-19 10:26:21 +0100

    MIG-1091 - Add test for wrong arguments to service constructors in xml

---
+++ tests/ServiceCorrectArgumentsTest.php
@@ -0,0 +1,87 @@
+<?php declare(strict_types=1);
+/*
+ * (c) shopware AG <info@shopware.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SwagMigrationAssistant\Test;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+use Shopware\Core\Framework\Log\Package;
+use Shopware\Core\Framework\Test\TestCaseBase\KernelTestBehaviour;
+use Symfony\Component\Finder\Finder;
+
+#[Package('services-settings')]
+class ServiceCorrectArgumentsTest extends TestCase
+{
+    use KernelTestBehaviour;
+
+    #[DataProvider('serviceProvider')]
+    public function testServiceShouldHaveCorrectArgumentsInContainer(string $xmlPath, string $serviceId): void
+    {
+        $service = $this->getContainer()->get($serviceId);
+        static::assertNotNull($service);
+    }
+
+    /**
+     * @return array<string, array{ xmlPath: string, serviceId: string }>
+     */
+    public static function serviceProvider(): array
+    {
+        $pluginPath = __DIR__ . '/../';
+        $finder = new Finder();
+        $finder->in($pluginPath)->files()->name('*.xml')->contains('<services>');
+
+        $testCases = [];
+        foreach ($finder->getIterator() as $xmlFile) {
+            $xmlPath = $xmlFile->getRealPath();
+            if (!$xmlPath) {
+                continue;
+            }
+
+            $services = self::getServicesFromXml($xmlPath);
+
+            foreach ($services as $serviceId) {
+                $testCases[$serviceId] = [
+                    'xmlPath' => $xmlPath,
+                    'serviceId' => $serviceId,
+                ];
+            }
+        }
+
+        return $testCases;
+    }
+
+    /**
+     * @return string[]
+     */
+    private static function getServicesFromXml(string $xmlPath): array
+    {
+        $xmlContent = file_get_contents($xmlPath);
+        static::assertNotFalse($xmlContent);
+        $document = new \DOMDocument();
+        $document->loadXML($xmlContent);
+
+        $serviceTags = $document->getElementsByTagName('service');
+
+        $serviceIds = [];
+        /** @var \DOMElement|\DOMNode|\DOMNameSpaceNode|null $element */
+        foreach ($serviceTags as $element) {
+            if ($element instanceof \DOMElement) {
+                $id = $element->getAttribute('id');
+                $abstract = $element->getAttribute('abstract');
+                if (\strtolower($abstract) === 'true') {
+                    // skipping abstract services,
+                    // because objects of them can't be constructed
+                    continue;
+                }
+
+                $serviceIds[] = $id;
+            }
+        }
+
+        return $serviceIds;
+    }
+}
