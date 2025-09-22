f19ad6ccb0b45ea69e85fea75351c0ac3dd8d8ec
Lars Kemper <l.kemper@shopware.com>
2025-09-22 11:42:50 +0200

    refactor: use meteor tokens

--- src/Resources/app/administration/src/module/swag-migration/component/index.ts
+++ src/Resources/app/administration/src/module/swag-migration/component/index.ts
@@ -6,7 +6,6 @@ import './card';
 import './loading-screen';
 
 Shopware.Component.register('swag-migration-dashboard-card', () => import('./swag-migration-dashboard-card'));
-Shopware.Component.register('swag-migration-expand-div', () => import('./swag-migration-expand-div'));
 Shopware.Component.extend('swag-migration-grid-extended', 'sw-grid', () => import('./swag-migration-grid-extended'));
 Shopware.Component.register('swag-migration-grid-selection', () => import('./swag-migration-grid-selection'));
 Shopware.Component.register('swag-migration-settings-icon', () => import('./swag-migration-settings-icon'));
