76975066fd88806ea2277f54d4fec33bd658fc05
Nikolas Evers <vintagesucks@users.noreply.github.com>
2025-11-24 08:11:29 +0000

    fix: support migrating email headers and footers (#87)

---
+++ tests/_fixtures/Shopware6/MailHeaderFooter/01-HappyCase/input.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+/*
+ * (c) shopware AG <info@shopware.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'id' => 'a1b2c3d4e5f64a7b8c9d0e1f2a3b4c5d',
+    'systemDefault' => false,
+    'translations' => [
+        [
+            'languageId' => '2fbb5fe2e29a4d70aa5854ce7ce3e20b',
+            'name' => 'Test Header Footer',
+            'description' => 'Test description',
+            'headerHtml' => '<div>Test HTML Header</div>',
+            'headerPlain' => 'Test Plain Header',
+            'footerHtml' => '<div>Test HTML Footer</div>',
+            'footerPlain' => 'Test Plain Footer',
+        ],
+    ],
+];
