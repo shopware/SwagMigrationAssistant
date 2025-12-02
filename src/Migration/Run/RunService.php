607e473ef4635be6e00ac8428b8b138f5a2dee06
Lars Kemper <l.kemper@shopware.com>
2025-12-02 14:21:20 +0100

    test: fix run service test

--- src/Migration/Run/RunService.php
+++ src/Migration/Run/RunService.php
@@ -23,7 +23,7 @@ use Shopware\Storefront\Theme\ThemeCollection;
 use Shopware\Storefront\Theme\ThemeDefinition;
 use Shopware\Storefront\Theme\ThemeService;
 use SwagMigrationAssistant\Exception\MigrationException;
-use SwagMigrationAssistant\Migration\Connection\Fingerprint\MigrationFingerprintService;
+use SwagMigrationAssistant\Migration\Connection\Fingerprint\MigrationFingerprintServiceInterface;
 use SwagMigrationAssistant\Migration\Connection\SwagMigrationConnectionCollection;
 use SwagMigrationAssistant\Migration\Connection\SwagMigrationConnectionEntity;
 use SwagMigrationAssistant\Migration\DataSelection\DataSelectionCollection;
@@ -75,7 +75,7 @@ class RunService implements RunServiceInterface
         private readonly MigrationContextFactoryInterface $migrationContextFactory,
         private readonly PremappingServiceInterface $premappingService,
         private readonly RunTransitionServiceInterface $runTransitionService,
-        private readonly MigrationFingerprintService $connectionFingerprintService,
+        private readonly MigrationFingerprintServiceInterface $connectionFingerprintService,
     ) {
     }
 
