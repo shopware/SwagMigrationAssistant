efd2c6fb052475a7098a65e466e301e3ff428e3c
Lars Kemper <l.kemper@shopware.com>
2026-02-17 09:03:36 +0100

    fix: customer & order address country state conversion

--- src/Profile/Shopware/Converter/CustomerConverter.php
+++ src/Profile/Shopware/Converter/CustomerConverter.php
@@ -492,6 +492,7 @@ abstract class CustomerConverter extends ShopwareConverter
 
         if ($countryStateUuid !== null) {
             $state['id'] = $countryStateUuid;
+            $state['shortCode'] = $oldAddressData['country']['countryiso'] . '-' . $oldAddressData['state']['shortcode'];
 
             return $state;
         }
@@ -528,7 +529,10 @@ abstract class CustomerConverter extends ShopwareConverter
 
         $this->applyCountryStateTranslation($state, $oldStateData);
         $this->convertValue($state, 'name', $oldStateData, 'name');
-        $this->convertValue($state, 'shortCode', $oldStateData, 'shortcode');
+
+        $state['shortCode'] = $oldAddressData['country']['countryiso'] . '-' . $oldStateData['shortcode'];
+        unset($oldStateData['shortcode']);
+
         $this->convertValue($state, 'position', $oldStateData, 'position', self::TYPE_INTEGER);
         $this->convertValue($state, 'active', $oldStateData, 'active', self::TYPE_BOOLEAN);
 
