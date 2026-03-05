e0b5550c58bf3f8dec6221dfeaaaf101a070aa7b
Jozsef Damokos <j.damokos@shopware.com>
2026-03-05 17:01:41 +0200

    change replace pattern

--- tests/acceptance/tests/MigrationTest.spec.ts
+++ tests/acceptance/tests/MigrationTest.spec.ts
@@ -280,8 +280,9 @@ test.describe('Migration Tests @migration @visual', () => {
             );
 
             // remove exception paths
-            logString = logString.replaceAll(/^Exception message:.*? in \/.*?\/src\//gm, (match) =>
-                match.replace(/ in \/.*?\/src\//, ' in [path]src/'),
+            logString = logString.replaceAll(
+                /^(Exception message:.*? in )\/.*?\/src\//gm,
+                '$1[path]src/',
             );
 
             expect(logString).toMatchSnapshot('migration-log-sw5.txt');
