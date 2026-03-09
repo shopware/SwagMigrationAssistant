0c4611c35fefec3eeecc78246f8acd23b4a29533
Jozsef Damokos <j.damokos@shopware.com>
2026-03-09 09:56:36 +0100

    Revert "fix exception"
    This reverts commit d26face7186638cd65e13217a7f44372af6877c2.

--- src/Profile/Shopware/Converter/ProductConverter.php
+++ src/Profile/Shopware/Converter/ProductConverter.php
@@ -380,9 +380,7 @@ abstract class ProductConverter extends ShopwareConverter
             unset($data['unit'], $data['detail']['unitID']);
         }
 
-        if (\is_array($data['prices'][0])) {
-            $converted['price'] = $this->getPrice($data['prices'][0], $converted['tax']['taxRate']);
-        }
+        $converted['price'] = $this->getPrice($data['prices'][0], $converted['tax']['taxRate']);
 
         if (empty($converted['price'])) {
             $this->loggingService->log(
@@ -396,9 +394,7 @@ abstract class ProductConverter extends ShopwareConverter
             );
         }
 
-        if (\is_array($data['prices'])) {
-            $converted['prices'] = $this->getPrices($data['prices'], $converted);
-        }
+        $converted['prices'] = $this->getPrices($data['prices'], $converted);
         unset($data['prices']);
 
         if (isset($data['assets'])) {
