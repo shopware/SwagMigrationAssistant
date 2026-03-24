1e38af28d593206be8c2ce93d1cba1e34f1c1249
Lars Kemper <l.kemper@shopware.com>
2026-03-24 09:32:07 +0100

    fix: phpunit

--- tests/Mock/Migration/Logging/DummyLoggingService.php
+++ tests/Mock/Migration/Logging/DummyLoggingService.php
@@ -7,14 +7,27 @@
 
 namespace SwagMigrationAssistant\Test\Mock\Migration\Logging;
 
+use Psr\Log\NullLogger;
 use Shopware\Core\Framework\Log\Package;
+use Shopware\Core\Test\Stub\DataAbstractionLayer\StaticEntityRepository;
 use SwagMigrationAssistant\Migration\Logging\LoggingService;
+use SwagMigrationAssistant\Migration\Logging\SwagMigrationLoggingCollection;
+use SwagMigrationAssistant\Migration\Logging\SwagMigrationLoggingDefinition;
+use SwagMigrationAssistant\Migration\MigrationConfiguration;
 
 #[Package('fundamentals@after-sales')]
 class DummyLoggingService extends LoggingService
 {
     public function __construct()
     {
+        /** @var StaticEntityRepository<SwagMigrationLoggingCollection> $repository */
+        $repository = new StaticEntityRepository([], new SwagMigrationLoggingDefinition());
+
+        parent::__construct(
+            $repository,
+            new NullLogger(),
+            new MigrationConfiguration(),
+        );
     }
 
     public function getLoggingArray(): array
