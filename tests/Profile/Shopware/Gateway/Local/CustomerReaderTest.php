05d40eb533f6e3351dc76146ac218541f6190d0f
Jozsef Damokos <j.damokos@shopware.com>
2026-06-23 11:48:50 +0300

    update tests

--- tests/Profile/Shopware/Gateway/Local/CustomerReaderTest.php
+++ tests/Profile/Shopware/Gateway/Local/CustomerReaderTest.php
@@ -7,6 +7,7 @@
 
 namespace SwagMigrationAssistant\Test\Profile\Shopware\Gateway\Local;
 
+use Doctrine\DBAL\Connection;
 use PHPUnit\Framework\TestCase;
 use Shopware\Core\Framework\Log\Package;
 use SwagMigrationAssistant\Migration\MigrationContext;
@@ -21,15 +22,20 @@ class CustomerReaderTest extends TestCase
 {
     use LocalCredentialTrait;
 
+    private const CUSTOMER_LANGUAGE_SHOP_ID = 32;
+
     private CustomerReader $customerReader;
 
     private MigrationContext $migrationContext;
+
+    private Connection $dbConnection;
 
     protected function setUp(): void
     {
         $this->connectionSetup();
 
-        $this->customerReader = new CustomerReader(new ConnectionFactory());
+        $connectionFactory = new ConnectionFactory();
+        $this->customerReader = new CustomerReader($connectionFactory);
 
         $this->migrationContext = new MigrationContext(
             $this->connection,
@@ -42,6 +48,44 @@ class CustomerReaderTest extends TestCase
         );
 
         $this->migrationContext->setGateway(new DummyLocalGateway());
+
+        $this->dbConnection = $connectionFactory->createDatabaseConnection($this->migrationContext);
+
+        $this->dbConnection->executeStatement(
+            'INSERT INTO s_core_shops (
+                id, name, position, hosts, secure, locale_id, customer_scope, `default`, active
+            ) VALUES (
+                :id, :name, :position, :hosts, :secure, :localeId, :customerScope, :default, :active
+            )',
+            [
+                'id' => self::CUSTOMER_LANGUAGE_SHOP_ID,
+                'name' => 'English shop',
+                'position' => 0,
+                'hosts' => '',
+                'secure' => 0,
+                'localeId' => 2,
+                'customerScope' => 0,
+                'default' => 0,
+                'active' => 1,
+            ]
+        );
+
+        $this->dbConnection->executeStatement('UPDATE s_user SET language = :language WHERE id = :id', [
+            'language' => self::CUSTOMER_LANGUAGE_SHOP_ID,
+            'id' => 1,
+        ]);
+    }
+
+    protected function tearDown(): void
+    {
+        $this->dbConnection->executeStatement('UPDATE s_user SET language = :language WHERE id = :id', [
+            'language' => 1,
+            'id' => 1,
+        ]);
+
+        $this->dbConnection->executeStatement('DELETE FROM s_core_shops WHERE id = :id', [
+            'id' => self::CUSTOMER_LANGUAGE_SHOP_ID,
+        ]);
     }
 
     public function testRead(): void
@@ -56,7 +100,7 @@ class CustomerReaderTest extends TestCase
         static::assertSame('1', $data[0]['default_billing_address_id']);
         static::assertSame('3', $data[0]['default_shipping_address_id']);
         static::assertSame('prepayment', $data[0]['defaultpayment']['name']);
-        static::assertSame('de-DE', $data[0]['customerlanguage']['locale']);
+        static::assertSame('en-GB', $data[0]['customerlanguage']['locale']);
         static::assertSame('0', $data[0]['shop']['customer_scope']);
         static::assertCount(2, $data[0]['addresses']);
 
@@ -74,7 +118,7 @@ class CustomerReaderTest extends TestCase
         static::assertSame('5', $data[2]['default_billing_address_id']);
         static::assertSame('5', $data[2]['default_shipping_address_id']);
         static::assertSame('prepayment', $data[2]['defaultpayment']['name']);
-        static::assertSame('ro-RO', $data[2]['customerlanguage']['locale']);
+        static::assertSame('de-DE', $data[2]['customerlanguage']['locale']);
         static::assertSame('0', $data[2]['shop']['customer_scope']);
         static::assertCount(1, $data[2]['addresses']);
     }
