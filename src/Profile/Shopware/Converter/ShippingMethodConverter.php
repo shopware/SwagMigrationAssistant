9aba3f21e2228a9828a7d9d37bad117448357aec
Michael Telgmann <m.telgmann@shopware.com>
2026-07-31 13:48:15 +0200

    rename shipping rule array shape alias

--- src/Profile/Shopware/Converter/ShippingMethodConverter.php
+++ src/Profile/Shopware/Converter/ShippingMethodConverter.php
@@ -30,7 +30,7 @@ use SwagMigrationAssistant\Profile\Shopware\Premapping\DefaultShippingAvailabili
 use SwagMigrationAssistant\Profile\Shopware\Premapping\DeliveryTimeReader;
 
 /**
- * @phpstan-type Rules array{id: string, name: string, priority: int, moduleTypes: array<string, array<string>>, conditions: array<mixed>}|array{}
+ * @phpstan-type ShippingMethodRule array{id: string, name: string, priority: int, moduleTypes: array<string, array<string>>, conditions: array<mixed>}|array{}
  * @phpstan-type MainOrContainer array{id: string, ruleId: string, type: string, position: int, children: list<array{id: string, ruleId: string, parentId: string, type: string, position: int, children?: list<array<string, mixed>>}>}
  */
 #[Package('fundamentals@after-sales')]
@@ -300,7 +300,7 @@ abstract class ShippingMethodConverter extends ShopwareConverter
     /**
      * @param array<string, mixed> $data
      *
-     * @return Rules
+     * @return ShippingMethodRule
      */
     protected function getCustomerGroupCalculationRule(array $data): array
     {
@@ -398,7 +398,7 @@ abstract class ShippingMethodConverter extends ShopwareConverter
     /**
      * @param array<string, mixed> $data
      *
-     * @return Rules
+     * @return ShippingMethodRule
      */
     protected function getSalesChannelCalculationRule(array $data): array
     {
@@ -457,7 +457,7 @@ abstract class ShippingMethodConverter extends ShopwareConverter
         $conditionUuid = (string) $mapping['entityId'];
         $this->mappingIds[] = $mapping['id'];
 
-        $rule = [
+        return [
             'id' => $priceRuleUuid,
             'name' => 'Sales channel: ' . $salesChannelName,
             'priority' => 0,
@@ -492,14 +492,12 @@ abstract class ShippingMethodConverter extends ShopwareConverter
                 ],
             ],
         ];
-
-        return $rule;
     }
 
     /**
      * @param array<string, mixed> $data
      *
-     * @return Rules
+     * @return ShippingMethodRule
      */
     protected function getSalesChannelAndCustomerGroupCalculationRule(array $data): array
     {
@@ -632,7 +630,7 @@ abstract class ShippingMethodConverter extends ShopwareConverter
 
     /**
      * @param array<string, mixed> $data
-     * @param Rules $rule
+     * @param ShippingMethodRule $rule
      *
      * @return list<array<string, mixed>>
      */
