375e19fd3576f082b12206df35231f1c4ecca320
Lars Kemper <l.kemper@shopware.com>
2026-08-12 08:16:16 +0200

    fix: tests

--- tests/Profile/Shopware6/Converter/LanguageConverterConvertDataTest.php
+++ tests/Profile/Shopware6/Converter/LanguageConverterConvertDataTest.php
@@ -91,6 +91,7 @@ class LanguageConverterConvertDataTest extends TestCase
         $context = Context::createDefaultContext();
         $migrationContext = $this->createMigrationContext();
         $mappingServiceMock = $this->createMock(MappingServiceInterface::class);
+        $mappingServiceMock->method('getOrCreateMapping')->willReturn(['id' => Uuid::randomHex()]);
 
         $languageLookupMock = $this->createMock(LanguageLookup::class);
         $languageLookupMock->method('getLanguageEntity')->willReturn(null);
@@ -118,6 +119,7 @@ class LanguageConverterConvertDataTest extends TestCase
         $migrationContext = $this->createMigrationContext();
 
         $mappingServiceMock = $this->createMock(MappingServiceInterface::class);
+        $mappingServiceMock->method('getOrCreateMapping')->willReturn(['id' => Uuid::randomHex()]);
 
         $languageLookupMock = $this->createMock(LanguageLookup::class);
         $languageLookupMock->method('getLanguageEntity')->willReturn(new LanguageEntity());
