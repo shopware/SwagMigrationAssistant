90ac6af538c20fbb133b06461c3a19188a95a431
Pierre02 <p.bernard@shopware.com>
2026-09-09 09:10:54 +0200

    fix: phpstan playwright build (#222)

--- tests/Jest/src/module/swag-migration/component/swag-migration-error-resolution/swag-migration-error-resolution-modal.spec.js
+++ tests/Jest/src/module/swag-migration/component/swag-migration-error-resolution/swag-migration-error-resolution-modal.spec.js
@@ -1,7 +1,7 @@
 /**
  * @sw-package after-sales
  */
-import { mount } from '@vue/test-utils';
+import { DOMWrapper, mount } from '@vue/test-utils';
 import SwagMigrationErrorResolutionModal, {
     ERROR_CODE_COMPONENT_MAPPING,
 } from 'SwagMigrationAssistant/module/swag-migration/component/swag-migration-error-resolution/swag-migration-error-resolution-modal';
@@ -442,8 +442,10 @@ describe('module/swag-migration/component/swag-migration-error-resolution/swag-m
 
             expect(wrapper.find('.swag-migration-error-resolution-details-modal').exists()).toBe(false);
 
-            expect(wrapper.find('.sw-context-button__menu-popover').exists()).toBe(true);
-            await wrapper.find('.swag-migration-error-resolution-modal__left-modal-action-details').trigger('click');
+            expect(new DOMWrapper(document.body).find('.sw-context-button__menu-popover').exists()).toBe(true);
+            await new DOMWrapper(document.body)
+                .find('.swag-migration-error-resolution-modal__left-modal-action-details')
+                .trigger('click');
 
             expect(wrapper.find('.swag-migration-error-resolution-details-modal').exists()).toBe(true);
             expect(wrapper.vm.selectedDetailsLog).toStrictEqual(wrapper.vm.tableData.at(0));
@@ -471,15 +473,21 @@ describe('module/swag-migration/component/swag-migration-error-resolution/swag-m
             await wrapper.find('.sw-data-grid__row--1 .sw-data-grid__cell--actions button').trigger('click');
             await flushPromises();
 
-            expect(wrapper.find('.swag-migration-error-resolution-modal__left-modal-action-reset').exists()).toBe(false);
+            expect(
+                new DOMWrapper(document.body)
+                    .find('.swag-migration-error-resolution-modal__left-modal-action-reset')
+                    .exists(),
+            ).toBe(false);
 
             await wrapper.find('.sw-data-grid__row--0 .sw-data-grid__cell--actions button').trigger('click');
             await flushPromises();
 
             migrationFixRepositoryMock.search.mockReturnValueOnce(Promise.resolve([]));
 
-            expect(wrapper.find('.sw-context-button__menu-popover').exists()).toBe(true);
-            await wrapper.find('.swag-migration-error-resolution-modal__left-modal-action-reset').trigger('click');
+            expect(new DOMWrapper(document.body).find('.sw-context-button__menu-popover').exists()).toBe(true);
+            await new DOMWrapper(document.body)
+                .find('.swag-migration-error-resolution-modal__left-modal-action-reset')
+                .trigger('click');
 
             await flushPromises();
 
@@ -888,7 +896,7 @@ describe('module/swag-migration/component/swag-migration-error-resolution/swag-m
             await relationField.find('.sw-entity-single-select__selection').trigger('click');
             await flushPromises();
 
-            await relationField.find('.sw-select-result').trigger('click');
+            await new DOMWrapper(document.body).find('.sw-select-result').trigger('click');
             await flushPromises();
 
             expect(wrapper.vm.fieldValue).toBe('tax-id-1');
